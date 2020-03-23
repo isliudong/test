19665ab8f50ecbf7c718b45df1568a938adc591a
liudong <2351036454@qq.com>
2020-03-23 20:04:05 +0800

    2

--- Hello World.java
+++ Hello World.java
@@ -1,6 +1,10 @@
 public class Hello{
 public static void main{
+System.out.println("Hello World3");
 System.out.println("Hello World1");
-System.out.println("Hello World");
+System.out.println("Hello World2");
+System.out.println("Hello World5");
+System.out.println("Hello World4");
+
 }
 }
